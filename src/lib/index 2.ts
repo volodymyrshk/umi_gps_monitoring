62c36963422b49d055a686009ee1b2c29ec15112
Volodymyr <volodymir.shkarban@gmail.com>
2025-09-11 01:38:45 -0400

    feat: Auto-commit by Gemini

---
+++ src/lib/index 2.ts
@@ -0,0 +1,21 @@
+/**
+ * Main library exports for U-Monitoring system
+ */
+
+// Entity types
+export * from './entities';
+
+// Telemetry system
+export * from './telemetry';
+
+// Data services
+export * from './services';
+
+// Validation
+export * from './validators';
+
+// Data generators (for development/testing)
+export * from './generators/vehicle-generator';
+
+// Utilities
+export * from './utils';
